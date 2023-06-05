e1c0300212189aabadc1fa2af2aa40fc60da2d7d
viktor472 <viktor.alex.ceder@gmail.com>
2023-06-05 09:00:59 +0200

    Data-added

---
+++ Data.js
@@ -0,0 +1,38 @@
+export const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d73",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d74",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d75",
+    title: "Fredrik",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at cursus arcu, vitae convallis felis.",
+    price: "200",
+  },
+];
